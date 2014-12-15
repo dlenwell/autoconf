e81505e6f63a9d878b2ed717cb7478c9331da496
david <dlenwell@gmail.com>
2014-12-15 15:15:42 -0800

    fixed a typo

--- autoconf.py
+++ autoconf.py
@@ -24,7 +24,7 @@ class AutoConf(dict):
         """"""
         self.data_to_output = {}
 
-        self.data_to_output = self.merge(default_config,location_config)
+        self.data_to_output = self.merge(default,location_config)
         self.data_to_output = self.merge(data_to_output,final_override)
 
         super(AutoConf, self).__init__(self.data_to_output)
