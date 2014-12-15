7f97e00c723102ef2d3672e54a0c835b441f4b54
david <dlenwell@gmail.com>
2014-12-15 14:53:51 -0800

    initial commit

---
+++ autoconf.py
@@ -0,0 +1,38 @@
+#
+# Copyright (c) 2015 Blue Box Group, LLC
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import types
+
+
+class AutoConf(dict):
+    """this class extends a dict and from the outside can behave like one."""
+
+    def __init__(self,default_config={} ,location_config={}, final_override={}):
+        """"""
+        data_to_output = {}
+        
+        data_to_output = self.merge(default_config,location_config)      
+        data_to_output = self.merge(data_to_output,final_override)        
+        super(AutoConf, self).__init__(data_to_output)
+
+    def merge(self,x,y):
+        """merges nested dictionary objects """
+        merged = dict(x,**y)
+        xkeys = x.keys()                     
+        for key in xkeys:
+            if type(x[key]) is types.DictType and y.has_key(key):
+                merged[key] = self.merge(x[key],y[key])
+
+        return merged
---
+++ example.py
@@ -0,0 +1,9 @@
+from autoconf import AutoConf
+
+foo = {'one': 'blah one', 'two': { 'three': 'blah three'}}
+bar = {'one': 'blah override', 'two' :{ 'seven': 'fart'}}
+arg = {'two' :{ 'seven': 'override'}}
+
+merged_config = AutoConf(foo, bar, arg)
+
+print merged_config
